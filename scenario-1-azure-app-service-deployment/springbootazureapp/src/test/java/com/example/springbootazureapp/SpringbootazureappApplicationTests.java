1501de0e16179911dd976b670a5298c230f324a2
purva jage <purvajage44@gmail.com>
2025-07-25 23:23:41 +0530

    add scenario1

---
+++ scenario-1-azure-app-service-deployment/springbootazureapp/src/test/java/com/example/springbootazureapp/SpringbootazureappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootazureapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootazureappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
