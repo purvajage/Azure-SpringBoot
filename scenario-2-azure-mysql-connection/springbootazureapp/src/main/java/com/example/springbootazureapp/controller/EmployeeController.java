8028706e289a7216985bb4af2722d88555906f1e
purva jage <purvajage44@gmail.com>
2025-07-29 22:01:14 +0530

    commit

--- scenario-2-azure-mysql-connection/springbootazureapp/src/main/java/com/example/springbootazureapp/controller/EmployeeController.java
+++ scenario-2-azure-mysql-connection/springbootazureapp/src/main/java/com/example/springbootazureapp/controller/EmployeeController.java
@@ -1,8 +1,38 @@
 package com.example.springbootazureapp.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.springbootazureapp.repository.EmployeeRepo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.example.springbootazureapp.model.Employee;
+import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 @RestController
+@RequestMapping("/employees")
 public class EmployeeController {
+@Autowired
+private EmployeeRepo employeeRepo;
+@GetMapping("/getEmployee")
+public List<Employee> getAll(){
+    return employeeRepo.findAll();
+}
+@PostMapping("/createEmployee")
+public Employee create(@RequestBody Employee employee) {
+    return employeeRepo.save(employee);
+}
+@PutMapping("/updateEmployee")
+public Employee update(@RequestBody Employee employee) {
+    return employeeRepo.save(employee);
+}
+@DeleteMapping("/deleteEmployee")
+public void delete(@RequestParam int id){
+    employeeRepo.deleteById((long) id);
+}
 
 }
 
--- scenario-2-azure-mysql-connection/springbootazureapp/src/main/java/com/example/springbootazureapp/repository/EmployeeRepo.java
+++ scenario-2-azure-mysql-connection/springbootazureapp/src/main/java/com/example/springbootazureapp/repository/EmployeeRepo.java
@@ -1,5 +1,6 @@
 package com.example.springbootazureapp.repository;
-
-public class EmployeeRepo {
+import com.example.springbootazureapp.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface EmployeeRepo extends JpaRepository<Employee,Long>{
 	
 }
