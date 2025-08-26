6277c757566e3277af57a56e82001c6948345e95
purva jage <purvajage44@gmail.com>
2025-08-26 17:44:49 +0530

    FIX:CONTROLLER

--- scenario-2-azure-mysql-connection/springbootazureapp/src/main/java/com/example/springbootazureapp/controller/EmployeeController.java
+++ scenario-2-azure-mysql-connection/springbootazureapp/src/main/java/com/example/springbootazureapp/controller/EmployeeController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
 @RestController
-
 @RequestMapping("/employees")
 public class EmployeeController {
 @Autowired
