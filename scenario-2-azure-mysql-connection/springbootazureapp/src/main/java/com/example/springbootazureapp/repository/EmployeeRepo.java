f1d17ecc6778b4d788afd5a98ee2308a515c5bcb
purva jage <purvajage44@gmail.com>
2025-08-29 23:46:23 +0530

    commit

--- scenario-2-azure-mysql-connection/springbootazureapp/src/main/java/com/example/springbootazureapp/repository/EmployeeRepo.java
+++ scenario-2-azure-mysql-connection/springbootazureapp/src/main/java/com/example/springbootazureapp/repository/EmployeeRepo.java
@@ -2,5 +2,4 @@ package com.example.springbootazureapp.repository;
 import com.example.springbootazureapp.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface EmployeeRepo extends JpaRepository<Employee,Long>{
-
 }
